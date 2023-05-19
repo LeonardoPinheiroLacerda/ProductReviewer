cca082bd6fb3718a08bcbff74527621352f88268
LeonardoPinheiroLacerda <leon.lacerda2015@gmail.com>
2023-05-19 12:53:53 -0300

    Primeiro commit

---
+++ src/main/java/com/leonardo/productreviewer/ProductReviewerApplication.java
@@ -0,0 +1,13 @@
+package com.leonardo.productreviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductReviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductReviewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonardo/productreviewer/ProductReviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardo.productreviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductReviewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
